d246086b2274dab5d72cfeb9bf4ca234948ba187
maxwellmuchiri <muchirimaxwel@gmail.com>
2022-12-07 08:56:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 62
+const num2= 1
+let multiply=(num1*num2)
 
-// Write your code here
+let random = Math.round(2)
+
+const num3 = 19
+const num4 = 5
+let mod= (num3%num4) 
+
+let max = (2, 6 , 8, 18, 20)
